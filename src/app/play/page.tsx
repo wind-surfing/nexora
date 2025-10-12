1903cc2cc8d46a0d89e3810481f7d4b30ac68112
wind-surfing <windddsurfing@gmail.com>
2025-10-12 10:26:08 +0545

    Add animations to flashcard layout; include new GIFs for enhanced user experience

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Button from "@/components/shared/Button";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   FaArrowLeft,
   FaArrowRight,
@@ -27,7 +27,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-function page() {
+function Page() {
   return (
     <>
       <main className="flex flex-row items-center justify-center h-[calc(100vh-64px)] w-full px-4">
@@ -51,7 +51,7 @@ function page() {
             </div>
           </header>
           <div className="flex flex-col items-center justify-center w-full h-[calc(100vh-128px)]">
-            <div className="flex flex-row items-center justify-between w-full h-full">
+            <div className="flex flex-row items-center justify-between w-full h-full gap-12">
               <main className="flex flex-col items-start justify-start w-5/8 h-full">
                 <section className="aspect-video w-full bg-primary text-white rounded-2xl m-4 px-4 flex flex-col items-start justify-start relative overflow-hidden">
                   <header
@@ -59,11 +59,47 @@ function page() {
                     className="w-full flex flex-row items-center justify-between absolute top-4 left-0 px-6"
                   >
                     <span className="flex flex-row items-center gap-2">
-                      <FaWandMagicSparkles className="text-xl" />
+                      <Popover>
+                        <PopoverTrigger>
+                          <Tooltip>
+                            <TooltipTrigger type="button">
+                              <FaWandMagicSparkles className="text-xl" />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>Get a Hint</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-80">
+                          <div className="grid gap-4">
+                            <div className="space-y-2">
+                              <h4 className="leading-none font-medium">
+                                Confirm
+                              </h4>
+                              <p className="text-muted-foreground text-sm">
+                                Are you sure you want to spend 1 Hint potion to
+                                get a hint
+                              </p>
+                            </div>
+                            <div className="grid gap-2">
+                              <div className="grid grid-cols-3 items-center gap-4">
+                                <Button type="button" title="Confirm"></Button>
+                              </div>
+                            </div>
+                          </div>
+                        </PopoverContent>
+                      </Popover>
                       <span className="sr-only">Hint Content</span>
                     </span>
                     <span className="flex flex-row items-center gap-2">
-                      <AiFillSound className="text-xl" />
+                      <Tooltip>
+                        <TooltipTrigger type="button">
+                          <AiFillSound className="text-xl" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Listen to the sound</p>
+                        </TooltipContent>
+                      </Tooltip>
                     </span>
                   </header>
                   <div className="h-full w-full flex flex-row items-center justify-center">
@@ -87,11 +123,11 @@ function page() {
                     <div className="h-6 w-24 rounded overflow-hidden border-2 border-gray-700 shadow-inner relative">
                       <div
                         className="h-full bg-primary/60 transition-all duration-300 absolute top-0 left-0"
-                        style={{ width: "50%" }}
+                        style={{ width: "100%" }}
                       ></div>
                       <div
                         className="h-full bg-primary/5 transition-all duration-300 absolute top-0 left-0"
-                        style={{ width: "80%" }}
+                        style={{ width: "130%" }}
                       ></div>
                       <span
                         className="absolute inset-0 flex items-center justify-center text-[#2A2A2A]"
@@ -172,6 +208,38 @@ function page() {
                   </span>
                 </section>
               </main>
+              <main className="flex flex-col items-start justify-start w-3/8 h-full">
+                <section className="h-full w-full text-white rounded-2xl m-4 px-4 flex flex-col items-start justify-start relative overflow-hidden">
+                  <div className="h-full w-full flex flex-row items-center justify-center">
+                    <Image
+                      src="/standing.gif"
+                      alt="Monster Image"
+                      layout="fill"
+                      objectFit="cover"
+                      style={{
+                        transform: "scaleX(-1)",
+                      }}
+                    />
+                  </div>
+                </section>
+                <section className="w-full m-4 p-4 flex flex-row items-center justify-center rounded-xl">
+                  <span className="flex flex-row items-center justify-center gap-2">
+                    <span className="text-lg">HP</span>
+                    <div className="h-6 w-24 rounded overflow-hidden border-2 border-gray-700 shadow-inner relative">
+                      <div
+                        className="h-full bg-destructive/60 transition-all duration-300 absolute top-0 left-0"
+                        style={{ width: "100%" }}
+                      ></div>
+                      <span
+                        className="absolute inset-0 flex items-center justify-center text-[#2A2A2A]"
+                        style={{ zIndex: 20 }}
+                      >
+                        100%
+                      </span>
+                    </div>
+                  </span>
+                </section>
+              </main>
             </div>
           </div>
         </section>
@@ -180,4 +248,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
