5e367eb4941f54c858bb082698fba505c5ebff97
wind-surfing <windddsurfing@gmail.com>
2025-10-16 18:30:41 +0545

    Update package versions and add framer-motion dependency; create new Page component for signal display

---
+++ src/app/signal/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-[calc(100vh-64px)] w-full relative overflow-hidden">
+      <div className="relative flex flex-col items-center justify-end w-40 h-[400px] rounded-b-3xl border-2 border-primary bg-gradient-to-t from-[#001233] via-[#003366] to-[#0055ff] overflow-hidden">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Page;
